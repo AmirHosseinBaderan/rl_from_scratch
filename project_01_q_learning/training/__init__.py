b997c9e9e92e90cb46823b341448b0f609b3a258
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-20 23:43:51 +0330

    Update __init__.py

--- project_01_q_learning/training/__init__.py
+++ project_01_q_learning/training/__init__.py
@@ -1,4 +1,5 @@
 from .checkpoint import CheckpointManager
 from .tensorboard_logger import TensorBoardLogger
+from .config import TrainingConfig
 
-__all__ = ['CheckpointManager', 'TensorBoardLogger']
+__all__ = ['CheckpointManager', 'TensorBoardLogger','TrainingConfig']
