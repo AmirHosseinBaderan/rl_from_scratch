750d707f3b45b8cf343b7f5a9b7df30861b109e6
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-21 00:27:35 +0330

    Create test_evaluate.py

---
+++ project_01_q_learning/tests/test_evaluate.py
@@ -0,0 +1,46 @@
+from agent import QLearningAgent
+from environment import GridWorld, Action
+from evaluation.evaluate import Evaluator
+
+
+def test_evaluation_reaches_goal():
+    obstacles = {
+        (0, 3),
+        (1, 1),
+        (1, 3),
+        (2, 3),
+        (3, 0),
+    }
+
+    env = GridWorld(
+        rows=5,
+        cols=5,
+        start=(0, 0),
+        goal=(4, 4),
+        obstacles=obstacles,
+    )
+
+    agent = QLearningAgent(
+        rows=5,
+        cols=5,
+    )
+
+    # Artificially define a known policy
+    agent.q_table[0, 0, Action.RIGHT] = 1
+    agent.q_table[0, 1, Action.RIGHT] = 1
+    agent.q_table[0, 2, Action.DOWN] = 1
+    agent.q_table[1, 2, Action.DOWN] = 1
+    agent.q_table[2, 2, Action.DOWN] = 1
+    agent.q_table[3, 2, Action.RIGHT] = 1
+    agent.q_table[3, 3, Action.DOWN] = 1
+    agent.q_table[4, 3, Action.RIGHT] = 1
+
+    evaluator = Evaluator(
+        env=env,
+        agent=agent,
+    )
+
+    result = evaluator.evaluate()
+
+    assert result.success is True
+    assert result.path[-1] == (4, 4)
