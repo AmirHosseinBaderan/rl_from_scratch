a9adce154269acf3ef1d2a7620b877b673e44510
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-21 23:51:16 +0330

    update : use direction instead np

--- snake_rl/environment/snake.py
+++ snake_rl/environment/snake.py
@@ -2,30 +2,27 @@ from dataclasses import dataclass
 
 import numpy as np
 
+from .direction import Direction
+
 
 @dataclass
 class Snake:
     body: list[np.ndarray]
-    direction: np.ndarray
+    direction: Direction
 
     def __post_init__(self) -> None:
         if not self.body:
             raise ValueError("Snake body cannot be empty.")
-
-        if self.direction.shape != (2,):
-            raise ValueError("Direction must have shape (2,).")
 
     @property
     def head(self) -> np.ndarray:
         return self.body[0]
 
     def move(self) -> None:
-        new_head = self.head + self.direction
+        new_head = self.head + self.direction.vector
+
         self.body.insert(0, new_head)
         self.body.pop()
 
-    def change_direction(self, direction: np.ndarray) -> None:
-        if direction.shape != (2,):
-            raise ValueError("Direction must have shape (2,).")
-
+    def change_direction(self, direction: Direction) -> None:
         self.direction = direction
