6ab0f6c12856f3fa25d32e55b760cef22127b3b7
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-21 23:50:12 +0330

    update : change for enum

--- snake_rl/environment/direction.py
+++ snake_rl/environment/direction.py
@@ -4,11 +4,11 @@ import numpy as np
 
 
 class Direction(Enum):
-    UP = np.array([0, 1])
-    DOWN = np.array([0, -1])
-    LEFT = np.array([-1, 0])
-    RIGHT = np.array([1, 0])
+    UP = (0, 1)
+    DOWN = (0, -1)
+    LEFT = (-1, 0)
+    RIGHT = (1, 0)
 
     @property
     def vector(self) -> np.ndarray:
-        return self.value.copy()
+        return np.array(self.value, dtype=np.int64)
