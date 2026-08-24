a140bf6fff3f7596f4d7ca2670a8c00d9d54d60f
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-24 23:57:31 +0330

    Update __init__.py

--- snake_rl/environment/__init__.py
+++ snake_rl/environment/__init__.py
@@ -6,6 +6,8 @@ from .environment import SnakeEnvironment
 from .reward import Reward
 from .state import SnakeState
 from .state_builder import StateBuilder
-from .action import Action,to_direction
+from .action import Action, to_direction
+from .transition import StepResult
 
-__all__ = ["Grid", "Snake","Food", "Direction","SnakeEnvironment","Reward","SnakeState","StateBuilder","Action","to_direction"]
+__all__ = ["Grid", "Snake", "Food", "Direction", "SnakeEnvironment", "Reward", "SnakeState", "StateBuilder", "Action",
+           "to_direction", "StepResult"]
