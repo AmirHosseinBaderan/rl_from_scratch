6c00a92ed962914d3865a23132007cf192a6e886
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-25 21:30:05 +0330

    Update __init__.py

--- snake_rl/agent/__init__.py
+++ snake_rl/agent/__init__.py
@@ -1,6 +1,6 @@
 from .q_network import QNetwork
 from .action_selector import EpsilonGreedySelector
-from .exprience import Experience
+from .experience import Experience
 from .reply_buffer import ReplayBuffer
 
 __all__ = ["QNetwork","EpsilonGreedySelector","Experience","ReplayBuffer"]
