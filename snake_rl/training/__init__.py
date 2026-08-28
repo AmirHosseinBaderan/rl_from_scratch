af87122d7285545eeecd74d148ca885b66970701
amirhossein <amir0hossin0pr@gmail.com>
2026-08-28 20:20:40 +0330

    Update __init__.py

--- snake_rl/training/__init__.py
+++ snake_rl/training/__init__.py
@@ -1,6 +1,9 @@
 from .episode import EpisodeResult, run_episode
+from .trainer import Trainer,TrainingResult
 
 __all__ = [
     'EpisodeResult',
-    'run_episode'
+    'run_episode',
+    'Trainer',
+    'TrainingResult',
 ]
