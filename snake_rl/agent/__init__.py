e4a7c7062320f93ffd2be28bde34d13716f35c3b
amirhossein <amir0hossin0pr@gmail.com>
2026-08-28 14:03:57 +0330

    Update __init__.py

--- snake_rl/agent/__init__.py
+++ snake_rl/agent/__init__.py
@@ -4,5 +4,6 @@ from .experience import Experience
 from .reply_buffer import ReplayBuffer
 from .batch import create_batch
 from .targets import compute_bellman_target
+from .q_values import select_action_q_values
 
-__all__ = ["QNetwork","EpsilonGreedySelector","Experience","ReplayBuffer","create_batch","compute_bellman_target"]
+__all__ = ["QNetwork","EpsilonGreedySelector","Experience","ReplayBuffer","create_batch","compute_bellman_target","select_action_q_values"]
